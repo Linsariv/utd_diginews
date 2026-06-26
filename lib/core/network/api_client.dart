045bbcc56030c0bd79a53844f12b4cb6ef442e69
Lintang <humankusut15@gmail.com>
2026-06-26 18:40:18 +0700

    Setup Dio API client

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -1,1 +1,22 @@
-// TODO: Setup dependency injection
+import 'package:dio/dio.dart';
+
+class ApiClient {
+  late final Dio dio;
+
+  ApiClient() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://gnews.io/api/v4/',
+        connectTimeout: const Duration(seconds: 15),
+        receiveTimeout: const Duration(seconds: 15),
+      ),
+    );
+
+    dio.interceptors.add(
+      LogInterceptor(
+        requestBody: true,
+        responseBody: true,
+      ),
+    );
+  }
+}
