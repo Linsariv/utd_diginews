0d23b1913ab10ddcf589eb66f13bf5a84e5c1b66
Lintang <humankusut15@gmail.com>
2026-06-27 08:17:53 +0700

    Register dependencies using GetIt

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,7 +1,36 @@
 import 'package:get_it/get_it.dart';
 
+import '../../data/datasources/news_remote_datasource.dart';
+import '../../data/datasources/news_remote_datasource_impl.dart';
+import '../../data/repositories/news_repository_impl.dart';
+import '../../domain/repositories/news_repository.dart';
+import '../../domain/usecases/get_news.dart';
+import '../../presentation/bloc/news/news_bloc.dart';
+import '../network/api_client.dart';
+
 final sl = GetIt.instance;
 
 Future<void> setupLocator() async {
-  // Register dependencies here
+  // Core
+  sl.registerLazySingleton<ApiClient>(() => ApiClient());
+
+  // DataSource
+  sl.registerLazySingleton<NewsRemoteDatasource>(
+    () => NewsRemoteDatasourceImpl(sl()),
+  );
+
+  // Repository
+  sl.registerLazySingleton<NewsRepository>(
+    () => NewsRepositoryImpl(sl()),
+  );
+
+  // UseCase
+  sl.registerLazySingleton(
+    () => GetNews(sl()),
+  );
+
+  // Bloc
+  sl.registerFactory(
+    () => NewsBloc(sl()),
+  );
 }
